2dc57ff2edb128c9f785a0bbd790e71e25e28631
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-23 22:28:01 +0530

    Update whois.py

--- userbyte/plugins/whois.py
+++ userbyte/plugins/whois.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2020 by Team Devcode(Ayan Ansari, Adnan Ahmad) < https://github.com/DevcodeOfficial >.
+#
+# This file is part of project < https://github.com/DevcodeOfficial/UserByte > 
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/DevcodeOfficial/UserByte/blob/master/LICENSE >
+#
+# All rights reserved.
+
 """Get info about the replied user
 Syntax: .whois"""
 
