6aee055ca1c1d9a539a9c4f636e05f4e58fbe685
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-23 13:03:02 +0530

    Add files via upload

---
+++ userbyte/__init__.py
@@ -0,0 +1,21 @@
+import os, sys, logging, importlib, asyncio
+from pathlib import Path
+from datetime import datetime
+from pyrogram import Client, Filters
+from userbyte.config import STRING_SESSION, API_ID, API_HASH, LGROUP
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.WARN)
+logger = logging.getLogger(__name__)
+
+__version__ = '0.0.1'
+__author__ = 'Team Devcode(Ayan Ansari, Adnan Ahmad)'
+__source__ = 'https://github.com/hasibulkabir/pyrouserbot'
+__copyright__ = f"Pyrouserbot v{__version__} Copyright (c) 2020 {__author__}"
+
+byte = Client(STRING_SESSION, api_id=API_ID, api_hash=API_HASH)
+
+LOGGER_GROUP = LGROUP
+LOG = True
+cmd = [".","!","#","$"]
---
+++ userbyte/config.py
@@ -0,0 +1,6 @@
+import os
+
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
