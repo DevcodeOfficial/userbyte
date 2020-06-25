b3f58e582ac8f77e640cd5bfe4bb074c1555ec9e
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-25 16:57:28 +0530

    Update test.py

--- userbyte/plugins/test.py
+++ userbyte/plugins/test.py
@@ -2,7 +2,7 @@ import pyrogram
 from pyrogram import Filters
 import os
 from userbyte import byte, cmd
-from userbyte.helpers.welcome_msg import wlcm_msg
+
 @byte.on_message(Filters.command('test', cmd) & Filters.me)
 async def wlcm(byte, message):
   file = "userbyte/helpers/welcome_msg.py"
@@ -11,4 +11,5 @@ async def wlcm(byte, message):
   file = open(f"{file}", "w")
   file.write(f"wlcm_msg = {input_str}")
   file.close()
-  await message.edit({wlcm_msg})
+  from userbyte.helpers.welcome_msg import wlcm_msg
+  await message.edit(wlcm_msg)
