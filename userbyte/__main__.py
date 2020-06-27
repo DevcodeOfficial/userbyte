e4d2e0aeec447dfebb73837e1cf83bac970040d4
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-27 10:38:10 +0530

    Update __main__.py

--- userbyte/__main__.py
+++ userbyte/__main__.py
@@ -23,3 +23,25 @@ async def main():
         print("\nUserbot Stopped\n")
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
+
+## Test ##
+try:
+	import os
+	from sqlalchemy import create_engine
+	from sqlalchemy.ext.declarative import declarative_base
+	from sqlalchemy.orm import sessionmaker, scoped_session
+	
+	DB_URL = os.environ.get("DATABASE_URL", None)
+	
+	def start() -> scoped_session:
+		engine = create_engine(DB_URL)
+		BASE.metadata.bind = engine
+		BASE.metadata.create_all(engine)
+		return
+	
+	scoped_session(sessionmaker(bind=engine, autoflush=False))
+	BASE = declarative_base()
+	SESSION = start()
+	logger.info('database is running')
+except:
+	logger.info('database is not running')
