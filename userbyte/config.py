6b763ab58677fa866e5c12cbacf26e6dee4dfda8
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-27 10:58:35 +0530

    Update config.py

--- userbyte/config.py
+++ userbyte/config.py
@@ -3,4 +3,5 @@ import os
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
+DB_URI = os.environ.get("DATABASE_URL", None)
 PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
