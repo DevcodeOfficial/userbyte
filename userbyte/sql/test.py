648f5557f2aa6eca837c0a3c41fc072667d52afe
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-27 11:50:18 +0530

    Update test.py

--- userbyte/sql/test.py
+++ userbyte/sql/test.py
@@ -7,11 +7,15 @@ from userbyte.sql import SESSION, BASE
 
 class Notes(BASE):
     __tablename__ = "notes"
+    chat_id = Column(String(14), primary_key=True)
     name = Column(UnicodeText, primary_key=True)
-    
-    def __init__(self, name):
+    d_message_id = Column(Integer)
+
+    def __init__(self, chat_id, name, d_message_id):
+        self.chat_id = str(chat_id)  # ensure string
         self.name = name
-        
+        self.d_message_id = d_message_id
+
     def __repr__(self):
         return "<Note %s>" % self.name
 
@@ -21,18 +25,18 @@ Notes.__table__.create(checkfirst=True)
 NOTES_INSERTION_LOCK = threading.RLock()
 
 
-def add_note_to_db(note_name):
+def add_note_to_db(chat_id, note_name, note_message_id):
     with NOTES_INSERTION_LOCK:
-        prev = SESSION.query(Notes).get((note_name))
+        prev = SESSION.query(Notes).get((str(chat_id), note_name))
         if prev:
             SESSION.delete(prev)
-        note = Notes(note_name)
+        note = Notes(str(chat_id), note_name, note_message_id)
         SESSION.add(note)
         SESSION.commit()
 
 
-def get_note(note_name):
+def get_note(chat_id, note_name):
     try:
-        return SESSION.query(Notes).get((note_name))
+        return SESSION.query(Notes).get((str(chat_id), note_name))
     finally:
         SESSION.close()
