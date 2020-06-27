478d416b40d5a0530e9362d671d6bbe8163b5115
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-27 10:42:28 +0530

    Update __main__.py

--- userbyte/__main__.py
+++ userbyte/__main__.py
@@ -13,18 +13,6 @@ from userbyte.plugins import ALL_MODULES
 import pyrogram
 from pyrogram import *
 
-for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("userbyte.plugins." + module_name)
-
-async def main():
-        await byte.start()
-        logger.info(f"\n××××××××××××××××××××××××××××××××××××××××××××××××××××××××\n××××××××××××× Boooom Your Userbot Is Alive ×××××××××××××\n××××××××××××× And Your Bot Version is: {__version__} ×××××××××××\n××××××××××××××××××××××××××××××××××××××××××××××××××××××××\n×× Created By: Team Devcode(Ayan Ansari, Adnan Ahmad) ××\n××××××××××××××××××××××××××××××××××××××××××××××××××××××××")
-        await byte.idle()
-        print("\nUserbot Stopped\n")
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
-
-## Test ##
 try:
 	import os
 	from sqlalchemy import create_engine
@@ -45,3 +33,15 @@ try:
 	logger.info('database is running')
 except:
 	logger.info('database is not running')
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("userbyte.plugins." + module_name)
+
+async def main():
+        await byte.start()
+        logger.info(f"\n××××××××××××××××××××××××××××××××××××××××××××××××××××××××\n××××××××××××× Buuuuum Your Userbot Is Alive ×××××××××××××\n××××××××××××× And Your Bot Version is: {__version__} ×××××××××××\n××××××××××××××××××××××××××××××××××××××××××××××××××××××××\n×× Created By: Team Devcode(Ayan Ansari, Adnan Ahmad) ××\n××××××××××××××××××××××××××××××××××××××××××××××××××××××××")
+        
+        await byte.idle()
+        print("\nUserbot Stopped\n")
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
