066a2c9c3021f3e07b9f95f17137b58b827661b5
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-28 21:19:12 +0530

    Add files via upload

---
+++ userbyte/plugins/__init__.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2020 by Team Devcode(Ayan Ansari, Adnan Ahmad) < https://github.com/DevcodeOfficial >.
+#
+# This file is part of project < https://github.com/DevcodeOfficial/UserByte > 
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/DevcodeOfficial/UserByte/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from userbyte import logger
+
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+logger.info("Plugins to load : {}\n".format(" × ".join(ALL_MODULES)))
+
+from userbyte import byte, cmd, get_help
+from pyrogram import Filters, Message
+
+@byte.on_message(Filters.command("help", cmd) & Filters.me)
+async def _alive(byte, message):
+	input = message.command[1]
+	if not input:
+		ALL_MODULES = sorted(__list_all_modules())
+		await message.edit("**🤖🔥 UserByte Helper\n\n😊 Syntax :** `.help plugin name` **to know about a single plugin\n\nAvailable Plugins :** `{}`".format("`  **>**  `".join(a)))
+	else:
+		try:
+			result = get_help(input)
+			await message.edit(f"📄 Help For **{input}** Plugin :\n{result}")
+		except:
+			await message.edit(f"😐 There is No {input} Plugin")
