3f7695ef6d14de9735883ac93c927e1a001af67d
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-28 09:47:51 +0530

    Update config.py

--- userbyte/config.py
+++ userbyte/config.py
@@ -1,7 +1,15 @@
 import os
 
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
+
 API_ID = os.environ.get("API_ID", None)
+
 API_HASH = os.environ.get("API_HASH", None)
+
 DB_URI = os.environ.get("DATABASE_URL", None)
+
 PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
+
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
