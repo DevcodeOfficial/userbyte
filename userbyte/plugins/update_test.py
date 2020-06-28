ee6b7171a10662184a5fa95e75b0eae43aee3c3d
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-28 11:54:42 +0530

    Create update_test.py

---
+++ userbyte/plugins/update_test.py
@@ -0,0 +1,21 @@
+from git import Repo
+from userbyte import byte, cmd
+
+@byte.on_message(Filters.command('update', cmd) & Filters.me)
+async def updater(byte, message):
+	
+	await message.edit("`Checking for updates, please wait....`")
+    
+    off_repo = 'https://github.com/Devcodeofficial/Userbyte.git'
+    
+    branch = repo.active_branch.name
+    
+    repo.create_remote('upstream', off_repo)
+    
+    ups_rem = repo.remote('upstream')
+    
+    ups_rem.fetch(branch)
+    
+    ups_rem.pull(branch)
+    
+    await message.edit('👍 Updating Started ! Restart Me After 2 Minutes')
