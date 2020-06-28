45e163d463c9837393328e2a4ed7670ad358fa8e
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-28 11:10:09 +0530

    Imporveeeeddddd

--- userbyte/plugins/logs.py
+++ userbyte/plugins/logs.py
@@ -1,7 +1,6 @@
-import pyrogram
+import pyrogram, os , requests, heroku3
 from pyrogram import Filters
 from userbyte import byte, cmd
-import heroku3
 from userbyte.config import HEROKU_APP_NAME, HEROKU_API_KEY
 
 @byte.on_message(Filters.command('logs', cmd) & Filters.me)
@@ -9,8 +8,12 @@ async def looogs(byte, message):
 	await message.edit('`🤟 Fetching Logs.....`')
 	heroku = heroku3.from_key(HEROKU_API_KEY)
 	app = heroku.app(HEROKU_APP_NAME)
+	data = app.get_log()
 	
 	with open('userbyte_logs.txt', 'w') as log:
-		log.write(app.get_log())
-		await byte.send_document(chat_id=message.chat.id, document='userbyte_logs.txt', caption='😁 Here Is Your Logs File')
+		log.write(data)
+		key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
+		url = f'https://nekobin.com/{key}'
+		await byte.send_document(chat_id=message.chat.id, document='userbyte_logs.txt', caption=f'😁 Here is Your Logs File\n [Click Here](url) to View on Nekobin')
 		await message.delete()
+		os.remove('userbyte_logs.txt')
