3f3118b8d7ae01737a51c762d1d7ce851aa0c868
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-29 12:28:31 +0530

    Update notes.py

--- userbyte/plugins/notes.py
+++ userbyte/plugins/notes.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, Filters, InlineKeyboardMarkup
 from userbyte import cmd, byte
-from userbyte.config import DB_URI
+from userbyte.config import DB_URI, CHANNEL_ID
 from userbyte.helpers.msg_types import get_note_type, Types, get_file_id
 
-TG_URI = -1001436299899
+CHANNEL_ID = TG_URI
 MAX_MESSAGE_LENGTH = 4096
 
 if DB_URI is not None:
