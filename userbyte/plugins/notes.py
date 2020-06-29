b0a2dda29fcb83546986acc057c2a186455e91b2
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-29 12:58:59 +0530

    Update notes.py

--- userbyte/plugins/notes.py
+++ userbyte/plugins/notes.py
@@ -4,7 +4,7 @@ from userbyte.config import DB_URI
 from userbyte.helpers.msg_types import get_note_type, Types, get_file_id
 import os
 
-TG_URI = os.environ.get("CHANNEL_ID", None)
+TG_URI = -1001436299899
 MAX_MESSAGE_LENGTH = 4096
 
 if DB_URI is not None:
